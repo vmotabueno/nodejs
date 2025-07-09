26c2193bf58e0b3f51ab2d27616c514c16e2f506
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-07-09 10:25:01 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -20,15 +20,30 @@ import http from 'node:http'
 // GET /users => Buscando usuários no banc-end
 // POST /users => Criar um usuário no back-end
 
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+// JSON (JavaScript Object Notation) - Formato de troca de dados
+
+const users = []
+
 const server = http.createServer((req, res) => {
    const { method, url } = req
 
    if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+     .setHeader('Content-Type', 'application/json')
+     .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return res.end('Criação de usuário')
+    users.push({
+     id: 1,
+     name: 'victor cruz',
+     email: 'victorcruz@example.com'
+
+    })
   }
 
     return res.end('Teste')
