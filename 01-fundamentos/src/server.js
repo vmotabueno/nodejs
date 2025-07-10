ff00b2017804365d246036f4cc09ca64309d4189
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-07-10 15:59:51 -0300

    Middleware JSON - adicionando parser de body e integrando ao server.js

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -1,70 +1,31 @@
 import http from 'node:http'
-
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-// JSON (JavaScript Object Notation) - Formato de troca de dados
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer(async(req, res) => {
-   const { method, url } = req
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
 
-    const buffers = []
+  await json(req, res) // deixa só isso, sem fazer parsing manual
 
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body
-  }
-
-
-   if (method === 'GET' && url === '/users') {
+  if (method === 'GET' && url === '/users') {
     return res
-     .setHeader('Content-Type', 'application/json')
-     .end(JSON.stringify(users))
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
     users.push({
-     id: 1,
-     name,
-     email,
-
+      id: 1,
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
   }
 
-    return res.writeHead(404).end()
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
-
