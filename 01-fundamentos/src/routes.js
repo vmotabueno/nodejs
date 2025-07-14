157e21f606f6c3756f62e098fd43b1f41a6a80ed
Victor Mota Bueno <victor.bueno@precato.com.br>
2025-07-14 12:32:16 -0300

    Rotas com parâmetros (Regex)

--- 01-fundamentos/src/routes.js
+++ 01-fundamentos/src/routes.js
@@ -33,7 +33,7 @@ export const routes = [
   },
   {
     method: 'DELETE',
-    path: buildRoutePath('/users/:id'),
+    path: buildRoutePath('/users/:id/groups/:groupId'),
     handler: (req, res) => {
       return res.end()
     }
--- 01-fundamentos/src/utils/build-route-path.js
+++ 01-fundamentos/src/utils/build-route-path.js
@@ -1,5 +1,9 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
+  const paramsWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
   console.log(Array.from(path.matchAll(routeParametersRegex)))
+  const pathRegex = new RegExp(`^${paramsWithParams}`)
+
+  return pathRegex
 }
